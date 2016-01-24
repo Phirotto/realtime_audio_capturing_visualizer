e44d20123ae9d06ef066354ee729b4722bd67780
Alexey Pupyshev <alexey.pupyshev@team.wrike.com>
2016-01-24 12:54:13 +0300

    Initial Commit

---
+++ realtime_audio_capturing_visualizer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  realtime_audio_capturing_visualizer
+//
+//  Created by Alexey Pupyshev on 24/01/16.
+//  Copyright © 2016 Alexey Pupyshev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
